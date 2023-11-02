c01d7d04df0d4c7a79517c1b32747c00da229030
cainytheslave <wajo432@gmail.com>
2023-11-02 11:46:23 +0100

    Day 7 continued

--- Day7/Main.hs
+++ Day7/Main.hs
@@ -8,21 +8,23 @@ import qualified Data.Char as Char
 main :: IO ()
 main = do  
     rules <- lines <$> readFile "input.txt"
-    putStrLn (show . sum . map countAll $ answers)
+    putStrLn (map parseRule $ rules)
+
+canStore :: String -> 
 
 parseRule :: String -> (String, [(Int, String)])
-parseRule s = let (color, contents) = Split.splitOn " bags contain " $ s
+parseRule s = let [color, contents] = Split.splitOn " bags contain " $ s
                   contentList = parseContents $ contents
               in (color, contentList)
 
 parseContents :: String -> [(Int, String)]
 parseContents s
   | s == "no other bags." = []
-  | otherwise = map parseBag $ s
+  | otherwise = map parseBag $ bags
   where bags = Split.splitOn ", " s
 
 parseBag :: String -> (Int, String)
-parseBag s = let bag = filter (/="bags") . words . filter (not . Char.isPunctuation) $ s
+parseBag s = let bag = filter (\w -> w /= "bags" && w /= "bag") . words . filter (not . Char.isPunctuation) $ s
              in (read . head $ bag, unwords . tail $ bag)
         
 
